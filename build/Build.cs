4219d2cbaa78af0071bf3a2e9a8beb31bf7edcf7
Christopher Jones <chrisjones170187@gmail.com>
2022-04-18 21:24:36 +0100

    Add gitversion support

--- build/Build.cs
+++ build/Build.cs
@@ -1,15 +1,12 @@
-using System;
-using System.Linq;
 using Nuke.Common;
 using Nuke.Common.CI;
 using Nuke.Common.Execution;
 using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
@@ -31,12 +28,11 @@ class Build : NukeBuild
 
     [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
+    [GitVersion] readonly GitVersion GitVersion;
 
     AbsolutePath SourceDirectory => RootDirectory / "src";
     AbsolutePath OutputDirectory => RootDirectory / "output";
-
     Project CommandingProject => Solution.GetProject("Devit.Commanding");
-
 
     Target Clean => _ => _
         .Before(Restore)
@@ -60,6 +56,9 @@ class Build : NukeBuild
             DotNetBuild(s => s
                 .SetProjectFile(Solution)
                 .SetConfiguration(Configuration)
+                .SetAssemblyVersion(GitVersion.AssemblySemVer)
+                .SetFileVersion(GitVersion.AssemblySemFileVer)
+                .SetInformationalVersion(GitVersion.InformationalVersion)
                 .EnableNoRestore());
         });
 
@@ -75,9 +74,11 @@ class Build : NukeBuild
         .Executes(() =>
         {
             DotNetPack(o => o.SetConfiguration(Configuration)
+                             .SetVersion(GitVersion.NuGetVersion)
                              .EnableNoBuild()
                              .EnableNoRestore()
                              .SetNoDependencies(true)
                              .SetOutputDirectory(OutputDirectory / "nuget"));
         });
+
 }
