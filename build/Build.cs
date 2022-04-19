1ac4d7867eb43e841b7b8f36b292d97dcbef8a61
Christopher Jones <8274496+cjonesuk@users.noreply.github.com>
2022-04-19 12:32:16 +0100

    Feature/versioning strategy (#1)
    Added GitVersion and Nuke builds that build nuget packages as well as the GitHub Action workflow configuration

--- build/Build.cs
+++ build/Build.cs
@@ -1,42 +1,48 @@
-using System;
 using System.Linq;
 using Nuke.Common;
 using Nuke.Common.CI;
+using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.Execution;
 using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
+[DotNetVerbosityMapping]
+[UnsetVisualStudioEnvironmentVariables]
 [CheckBuildProjectConfigurations]
 [ShutdownDotNetAfterServerBuild]
+[GitHubActions(
+    "ci",
+    GitHubActionsImage.WindowsLatest,
+    AutoGenerate = true,
+    OnPushBranches = new[] { "main", "feature" },
+    OnPullRequestBranches = new[] { "feature" },
+    InvokedTargets = new[] { nameof(GitHubActions) },
+    ImportSecrets = new[] { "NugetApiKey" })]
 class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
-
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main() => Execute<Build>(x => x.Default);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
+    [Parameter] string NugetApiUrl = "https://api.nuget.org/v3/index.json"; //default
+    [Parameter] string NugetApiKey;
+
     [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
+    [GitVersion] readonly GitVersion GitVersion;
 
     AbsolutePath SourceDirectory => RootDirectory / "src";
     AbsolutePath OutputDirectory => RootDirectory / "output";
-
+    AbsolutePath NugetDirectory => OutputDirectory / "nuget";
     Project CommandingProject => Solution.GetProject("Devit.Commanding");
-
 
     Target Clean => _ => _
         .Before(Restore)
@@ -60,6 +66,9 @@ class Build : NukeBuild
             DotNetBuild(s => s
                 .SetProjectFile(Solution)
                 .SetConfiguration(Configuration)
+                .SetAssemblyVersion(GitVersion.AssemblySemVer)
+                .SetFileVersion(GitVersion.AssemblySemFileVer)
+                .SetInformationalVersion(GitVersion.InformationalVersion)
                 .EnableNoRestore());
         });
 
@@ -75,9 +84,41 @@ class Build : NukeBuild
         .Executes(() =>
         {
             DotNetPack(o => o.SetConfiguration(Configuration)
+                             .SetVersion(GitVersion.NuGetVersion)
                              .EnableNoBuild()
                              .EnableNoRestore()
                              .SetNoDependencies(true)
-                             .SetOutputDirectory(OutputDirectory / "nuget"));
+                             .SetOutputDirectory(NugetDirectory));
         });
+
+    Target Push => _ => _
+       .DependsOn(Pack)
+       .Requires(() => NugetApiUrl)
+       .Requires(() => NugetApiKey)
+       .Requires(() => Configuration.Equals(Configuration.Release))
+       .Executes(() =>
+       {
+           var files = GlobFiles(NugetDirectory, "*.nupkg")
+                .Where(filename => !filename.EndsWith("symbols.nupkg"))
+                .ToArray();
+
+           Assert.NotEmpty(files);
+
+           files.ForEach(filename =>
+           {
+               DotNetNuGetPush(s => s
+                   .SetTargetPath(filename)
+                   .SetSource(NugetApiUrl)
+                   .SetApiKey(NugetApiKey)
+               );
+           });
+       });
+
+    Target GitHubActions => _ => _
+        .DependsOn(Push)
+        .Executes();
+
+    Target Default => _ => _
+        .DependsOn(Pack)
+        .Executes();
 }
